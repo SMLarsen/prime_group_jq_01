0f9c06d28f639ee69d7ea657c88e8baf2eca1379
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2016-11-02 17:07:26 -0500

    added append function and created initial styles

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-function randomNumber(min, max) {
-	return Math.floor(Math.random() * (1 + max - min) + min);
-}
+$(document).ready(function() {
+	var fruitArray = ["apple", "orange", "banana", "grape"];
+
+	function fruitDisplayBuilder(fruit) {
+		$('.fruit-store-display').append('<div class="fruit"><div class="inner-fruit-container"><button>Buy</button><p>Avg. Price: <span id="avg-' + fruit + '-price"></span></div></div>');
+		$('.fruit:last').addClass(fruit);
+		$('.fruit:last').css({'background': 'url(img/' + fruit + '.jpg) no-repeat center center fixed', '-webkit-background-size': 'cover', '-moz-background-size': 'cover',
+	  '-o-background-size': 'cover','background-size': 'cover'});
+	}
+	for (var i = 0; i < fruitArray.length; i++) {
+		fruitDisplayBuilder(fruitArray[i]);
+	}
+});
+//  function Fruit(fruitType, startPrice){
+//     this.type = fruitType;
+//     this.startPrice = startPrice;
+//     this.currentVal = function() {};
+//     this.quantSold = function(){};
+//     this.totalSoldVal = function(){};
+// }
+//
+// var Customer = {
+//     initialCash:
+//     currentCash:
+//     fruitInventory: [][]
+// };
+//
+// function getPrice(startPrice) {
+//   var newPrice = 0;
+//   newPrice = randomNumber(startPrice - 0.5, startPrice + 0.5).toFixed(2);
+//   newPrice = Math.min(newPrice, 9.99);
+//   newPrice = Math.max(newPrice, 0.5);
+//   return newPrice;
+// }
+//
+// function randomNumber(min, max){
+//   return Math.random() * (1 + max - min) + min;
+// }
